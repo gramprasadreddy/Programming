78c36adeffacb76f68a72acc986f4c4aee505ba9
Ram <gajjalaramprasadreddy@gmail.com>
2024-03-29 15:41:03 -0500

    Valid Anagram

--- java/java/src/main/java/roadmap/arrays/ValidAnagram.java
+++ java/java/src/main/java/roadmap/arrays/ValidAnagram.java
@@ -1,6 +1,6 @@
 package roadmap.arrays;
 
-import java.lang.reflect.Array;
+
 import java.util.Arrays;
 
 class ValidAnagaram {
@@ -24,9 +24,8 @@ class ValidAnagaram {
 
 
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
-        ValidAnagaram s = new ValidAnagaram();
 
         System.out.println(new ValidAnagaram().isValidAnagram("anagram","nagaram"));
 
