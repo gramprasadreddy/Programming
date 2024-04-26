04a6763b8a28a19a27fa30b1a1c09aa80f0523bd
Ram <gajjalaramprasadreddy@gmail.com>
2024-04-26 13:41:48 -0500

    string reverse by using string builder inbuilt function to reverse

--- Java_Programming/StringReverse.java
+++ Java_Programming/StringReverse.java
@@ -14,9 +14,21 @@ class StringReverse{
 	return strBuilder.toString();
 	}
 
+	public static String reverse2(String str){
+		if(str == null || str.isEmpty()){
+			return str;
+		}else{
+			return new StringBuilder(str).reverse().toString();
+		}
+	
+
+	}
+
 	public static void main(String[] args){
 		
 		System.out.println(reverse("Hello"));
 		System.out.println(reverse("SAS"));
+		System.out.println(reverse2("Hello"));
+		System.out.println(reverse2("SAS"));
 	}
 }
